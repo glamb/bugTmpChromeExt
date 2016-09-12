a264156ac7f690ea41ebb08c7326f8c5e8c6ddaa
Gregg Lamb <glamb@Greggs-MacBook-Pro.local>
2016-09-12 14:33:24 -0700

    fix spacing

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
   chrome.tabs.executeScript({
-	    code: 'var text = "h3. {color:#59afe1} *Environment* {color}\\n\\n h3. {color:#59afe1} *Setup* {color}\\n\\n h3. {color:#59afe1} *Steps* {color}\\n\\n h3. {color:#59afe1} *Result* {color}\\n\\n h3. {color:#59afe1} *Expected* {color}\\n\\n h3. {color:#59afe1} *Notes* {color}";var description = document.getElementById("description");description.value = description.value + text;'
+	    code: 'var text = "h3. {color:#59afe1} *Environment* {color}\\n\\nh3. {color:#59afe1} *Setup* {color}\\n\\nh3. {color:#59afe1} *Steps* {color}\\n\\nh3. {color:#59afe1} *Result* {color}\\n\\nh3. {color:#59afe1} *Expected* {color}\\n\\nh3. {color:#59afe1} *Notes* {color}";var description = document.getElementById("description");description.value = description.value + text;'
 	  });
 });
